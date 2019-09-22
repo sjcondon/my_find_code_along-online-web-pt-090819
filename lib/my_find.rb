9d9d8e64b36dcf2de2e968eb39c6e6216cd3fa4d
sarah <s.j.condon01@gmail.com>
2019-09-22 03:49:29 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,8 +2,10 @@ require 'pry'
 
 def my_find(collection)
   i = 0
-  while i < 100
-    yield(1)
+  while i < collection.length
+    if yield(collection[i])
+        return collection[i]
+  end
     i = i + 1
   end
 end
